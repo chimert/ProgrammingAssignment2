b8acb2a0e488aedc4015ae0f5b8772dd042d438e
chimert <chimert@users.noreply.github.com>
2014-04-27 13:44:37 +0200

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for creating cache inverse Matrix
 
-## Write a short comment describing this function
+## This function should create a cache of an inverse Matrix in the Global
+## It takes a matrix as an input and creates a set of functions available
+## to call or set that inverse matrix
+## environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        ## Function set, will specify the object
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        ## Function get, will return the object saved by 'set'
+        get <- function() x
+        ## Function setsolve, will specify the value of the inverse matrix
+        setsolve <- function(solve) m <<- solve
+        ## Function getsolve, will return the value of the inverse matrix
+        getsolve <- function() m
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)  
 }
 
 
-## Write a short comment describing this function
+## This function will return the inverse of 'x' if it is in the cache
+## otherwise, calculate it and return the value, store it in the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    
+        
+        m <- x$getsolve()
+        ## If the value of m is in the cache, return it from memory
+        if(!is.null(m)) {
+                message("getting inverse matrix from cache")
+                return(m)
+        }
+        ## If there is no cache, calculate it and return the value
+        data <- x$get()
+        m <- solve(data, ...)
+        ## Set the value into the cache
+        x$setsolve(m)
+        m
 }
